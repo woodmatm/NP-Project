3e5243b7d888682a614d5f0529cbbaa8176292da
joedrake26 <drakejc@dukes.jmu.edu>
2022-04-27 15:57:31 -0400

    Updated exact solution and new test case

--- exact-solution/max-knapsack.py
+++ exact-solution/max-knapsack.py
@@ -3,7 +3,7 @@
 
     Author: Joe Drake                  
 """
-
+import time
 
 def fillPack (W, val, weight, num, n):
     
@@ -14,13 +14,9 @@ def fillPack (W, val, weight, num, n):
         return fillPack(W, val, weight, num, n-1)
     
     else:
-        if(num[n-1] == 1):
-            return max(val[n-1] + fillPack(W-weight[n-1], val, weight, num, n-1),
-                fillPack(W, val, weight, num, n-1))
-        else:
-            num[n-1] = num[n-1]-1
-            return max(val[n-1] + fillPack(W-weight[n-1], val, weight, num, n),
-                fillPack(W, val, weight, num, n))
+        return max(val[n-1] + fillPack(W-weight[n-1], val, weight, num, n-1),
+            fillPack(W, val, weight, num, n-1))
+        
 
 
 
@@ -33,18 +29,28 @@ def main():
     W = int(input())
     n = int(input())
     items = [input().split() for _ in range(n)]
-    val = [0]*n
-    weight = [0]*n
-    num = [0]*n
+    
+    total = 0
     for element in items:
         element[1]=int(element[1])
         element[2]=int(element[2])
         element[3]=int(element[3])
+        total = total + element[3]
+    #Sort(items)
+    val = [0]*total
+    weight = [0]*total
+    num = [0]*total
+
+    temp = 0
     for i in range(n):
-        val[i] = items[i][1]
-        weight[i] = items[i][2]
-        num[i] = items[i][3]
-    print(fillPack(W, val, weight, num, n))
+        for j in range(items[i][3]):
+            val[temp] = items[i][1]
+            weight[temp] = items[i][2]
+            num[temp] = items[i][3]
+            temp += 1
+    t0 = time.time()
+    print(fillPack(W, val, weight, num, total))
+    print(time.time()-t0)
 
 
 if __name__ == "__main__":
