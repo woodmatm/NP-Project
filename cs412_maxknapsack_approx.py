30f37f0ccb3062a82ccc6251141d0a14f5dcf525
pglebus <72746873+pglebus@users.noreply.github.com>
2022-04-28 17:07:07 -0400

    Add files via upload

--- cs412_maxknapsack_approx.py
+++ cs412_maxknapsack_approx.py
@@ -1,12 +1,12 @@
 import math
 
 
+#This is a greedy algorithm. The greedy choice is sorting the items by value/weight ratio.
+#The source of this algorithm is found here under Greedy approximation algorithm: https://en.wikipedia.org/wiki/Knapsack_problem
+
+
 def main():
     #assume all items fit can individual fit into knapsack (ie no item exceeds knapsack weight)
-    #greedily start packing items by weight ratio (S1)
-    #make a second solution, with the first item that did not fit
-    #The union of S1 and S2  provides an upper bound, one of the sets has m/2.
-    #Return whichever set has a better value to btain a 1/2 approx
     capacity = int(input())
     num_obj = int(input())
     list_of_objects = []
@@ -23,39 +23,36 @@ def main():
     print(y)
 
 def approx(list_of_objects, capacity, num_obj):
-    list_of_objects.sort(key=sort_by_weight)
+    list_of_objects.sort(key=sort_by_weight)    #sort the items by weight/value ratio
     list_of_objects.reverse()
-    print(list_of_objects)
     cap = capacity
-    final_val = 0
     final_obj_list1 = []
     final_obj_list2 = []
     k_start = 0
     list1_value = 0
     list2_value = 0
     first_not_fit = False
-    for i in range(0, num_obj):
-        for j in range(0, list_of_objects[i][3]):
-            if cap - list_of_objects[i][2] >= 0:
+    for i in range(0, num_obj): #for all items, starting with the items with the highest weight/value ratio
+        for j in range(0, list_of_objects[i][3]): #begin to all the items of that type first (as many as is allowed)
+            if cap - list_of_objects[i][2] >= 0: #if the bag's capacity is exceeded, stop
                 final_obj_list1.append(list_of_objects[i])
                 cap -= list_of_objects[i][2]
-                list1_value += list_of_objects[i][1]
+                list1_value += list_of_objects[i][1] #add the item to the first bag, reduce bag capacity, keep track of total value of bag
             else:
-                if not first_not_fit:
+                if not first_not_fit: #if this is the first item that does not fit into the bag, keep track of its index
                     k_start = i
                     first_not_fit = True
-    final_obj_list2.append(list_of_objects[k_start])
-    num_obj -= 1
+    final_obj_list2.append(list_of_objects[k_start]) #add the first item that did not fit in to a new bag
     list2_value += list_of_objects[k_start][1]
     cap = capacity - list_of_objects[k_start][2]
-    list_of_objects.remove(list_of_objects[k_start])
+    list_of_objects[k_start][3] -= 1 #take out the item placed into the bag so it does not get added again
     for i in range(0, num_obj):
         for j in range(0, list_of_objects[i][3]):
             if cap - list_of_objects[i][2] >= 0:
                 final_obj_list2.append(list_of_objects[i])
                 cap -= list_of_objects[i][2]
                 list2_value += list_of_objects[i][1]
-    if list1_value > list2_value:
+    if list1_value > list2_value: #return whichever set has a higher value
         return final_obj_list1, list1_value
     else:
         return final_obj_list2, list2_value
