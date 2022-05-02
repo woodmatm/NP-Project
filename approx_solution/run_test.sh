6c0e276cb40db109ee046967f3387842e49c60ce
pglebus <72746873+pglebus@users.noreply.github.com>
2022-05-01 21:41:09 -0400

    Add files via upload

---
+++ approx_solution/run_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_0.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_1.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_2.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_3.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_4.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_5.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_6.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_7.txt
+python3 cs412_maxknapsack_approx.py < test_cases_approx/test_case_8.txt
